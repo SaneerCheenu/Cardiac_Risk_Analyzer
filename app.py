34c110ff6383be25faa62c24b3dec55e88fbd308
Saneer Wadhwa <94807999+SaneerCheenu@users.noreply.github.com>
2024-04-24 03:45:36 +0530

    Made result html & its css

--- app.py
+++ app.py
@@ -68,7 +68,6 @@ def predict():
             ca = int(request.form['ca'])
             thal = request.form.get('thal')
             
-            # Proceed with prediction
             data = np.array([[age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,slope,ca,thal]])
             my_prediction = model.predict(data)
             
